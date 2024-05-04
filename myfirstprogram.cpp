8f36fb424924cadad15ada10a36de06e1f74f33f
Emanuel Padilla <emanuehl159@gmail.com>
2024-05-04 15:50:21 -0500

    Add my first program in c++

---
+++ myfirstprogram.cpp
@@ -0,0 +1,17 @@
+/* iostream es usado como encabezado del archivo, para indicar que se pueden
+ * usar y retornar objetos */
+#include <iostream>
+/* using namespace std se usa para permitir implementar el nombre de std en el
+ * archivo */
+using namespace std;
+
+/* Declaración de la función */
+int main() {
+  /* cout en combinación con el operador de inserción (<<) es usado para
+  retornar / imprimir texto */
+  cout << "Hello World Jejeje :) \n"; // escape sequence = \n
+  /* Ese salto de linea se parece al de JS jeje*/
+  cout << "This is my first program in C++ !" << endl;
+  /* endl es otra forma de introducir un line break*/
+  return 0;
+}
