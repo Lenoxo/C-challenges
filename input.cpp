854340ab4ca699ad6a7ea922722fb4e163e3117a
Emanuel Padilla <emanuehl159@gmail.com>
2024-05-10 23:34:39 -0500

    Add practice with calculator

--- input.cpp
+++ input.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int sum(int num1, int num2) { return num1 + num2; }
+
 int main() {
   string input;
+  cout << "Write whatever you want: ";
   cin >> input;
   cout << "What you typed is: " << input << endl;
+  int input1, input2;
+  cout << "Write the first number: ";
+  cin >> input1;
+  cout << "Write the second number: ";
+  cin >> input2;
+  cout << "The result is: " << sum(input1, input2) << endl;
   return 0;
 }
