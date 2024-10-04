7cb356c1f7d3e362f0b21cd6accfc8f63881975a
Emanuel Padilla <emanuehl159@gmail.com>
2024-10-04 18:04:35 -0500

    Add small examples working with data types in C

---
+++ data-types/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  int number1 = 20;
+  // It can store up to 6 - 7 decimal digits - Takes up 4 bytes
+  float float1 = 13.33;
+  // It can store up to 15 decimal digits - Takes up 8 bytes
+  double float2 = 13.44;
+  char singleCharacter = 'E';
+
+  char myFirstString[] = "Hello there!";
+  char ascii = 67;
+
+  printf("%i\n", number1); // %i and %d work
+  printf("%f\n", float1);
+  printf("%lf\n", float2); // %lf is just used for double data type
+  printf("%c\n", singleCharacter);
+  printf("%s\n", myFirstString); // %s is the format specifier for strings
+  // You can specify the decimal precision using a dot (.)
+  printf("%.2f\n", float1);
+
+  // %lu is the format specifier for Long Unsigned
+  printf("Size of an int: %lu \n", sizeof(number1));
+  printf("Size of a float: %lu \n", sizeof(float1));
+  printf("Size of a double: %lu \n", sizeof(float2));
+  printf("Size of a char: %lu \n", sizeof(ascii));
+  printf("Size of a string: %lu \n", sizeof(myFirstString));
+
+  return 0;
+}
