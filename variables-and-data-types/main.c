56914bee26336bf334f527e2989ebf9f9f2661a0
Emanuel Padilla <emanuehl159@gmail.com>
2024-10-05 13:30:10 -0500

    Add small example working with booleans

--- variables-and-data-types/main.c
+++ variables-and-data-types/main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int main() {
@@ -11,6 +12,9 @@ int main() {
   char myFirstString[] = "Hello there!";
   char ascii = 67;
 
+  // To use bool type you have to import the header file stdbool.h
+  bool isPizzaTasty = true;
+
   printf("%i\n", number1); // %i and %d work
   printf("%f\n", float1);
   printf("%lf\n", float2); // %lf is just used for double data type
@@ -19,12 +23,15 @@ int main() {
   // You can specify the decimal precision using a dot (.)
   printf("%.2f\n", float1);
 
+  printf("This is a boolean value (Either 1 or 0): %d\n", isPizzaTasty);
+
   // %lu is the format specifier for Long Unsigned
   printf("Size of an int: %lu \n", sizeof(number1));
   printf("Size of a float: %lu \n", sizeof(float1));
   printf("Size of a double: %lu \n", sizeof(float2));
   printf("Size of a char: %lu \n", sizeof(ascii));
   printf("Size of a string: %lu \n", sizeof(myFirstString));
+  printf("Size of a bool: %lu \n", sizeof(isPizzaTasty));
 
   // Type conversion
 
@@ -38,7 +45,7 @@ int main() {
   // Constants - Same as JS xD
   const int MEANINGOFLIFE = 42; // I'll think a better joke later...
 
-  printf("Meaning of life: %i", MEANINGOFLIFE);
+  printf("Meaning of life: %i\n", MEANINGOFLIFE);
 
   return 0;
 }
