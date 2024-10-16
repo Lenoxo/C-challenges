1021f4c227b0d2e3d96b50e722e148126493255f
Emanuel Padilla <emanuehl159@gmail.com>
2024-10-16 12:46:07 -0500

    Add example with fibonnaci practicing recursion in C

--- functions/main.c
+++ functions/main.c
@@ -6,14 +6,21 @@ int sumTwoNums(int x, int y);
 int getLocalSenseOfTheUniverse();
 void greetings(char name[]);
 // The functions scope can be global or local
-
 int globalSenseOfTheUniverse = 42;
 
+int fibonacci(int num);
+int reps = 6;
 int main() {
   printf("Sum result: %i\n", sumTwoNums(33, 44));
   printf("Global sense: %i, Local sense: %i\n", globalSenseOfTheUniverse,
          getLocalSenseOfTheUniverse());
   greetings("Gustavo");
+  printf("%i is the result of using the fibonacci sequence %i times\n",
+         fibonacci(reps), reps);
+  for (int i = 0; i < reps; i++) {
+    printf("%i ", fibonacci(i));
+  }
+  printf("\n");
   return 0;
 }
 
@@ -28,3 +35,14 @@ int getLocalSenseOfTheUniverse() {
 }
 
 void greetings(char name[]) { printf("Hello, how are you today %s?\n", name); }
+
+int fibonacci(int num) {
+  if (num == 0) {
+    return 0;
+  }
+  if (num == 1) {
+    return 1;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
