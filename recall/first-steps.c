38ebf6678c20167d9850015d24bc9cc3012179d0
Emanuel Padilla <emanuehl159@gmail.com>
2024-11-06 23:51:19 -0500

    Add simple sum in recall.c

--- recall/first-steps.c
+++ recall/first-steps.c
@@ -1,3 +1,9 @@
 #include "stdio.h"
 
-int main() { printf("hello there\n"); }
+int num1 = 32;
+int num2 = 10;
+
+int main() {
+  printf("hello there\n");
+  printf("%d sense of the universe", num1 + num2);
+}
