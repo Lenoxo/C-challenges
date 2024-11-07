1e11987d2773aeb14001a43efda07e84837fdb40
Emanuel Padilla <emanuehl159@gmail.com>
2024-11-06 21:56:09 -0500

    Add example with double data type in recall.c

--- recall/first-steps.c
+++ recall/first-steps.c
@@ -3,7 +3,10 @@
 int num1 = 32;
 int num2 = 10;
 
+double decimalGrande = 3.333;
+
 int main() {
   printf("hello there\n");
   printf("%d sense of the universe", num1 + num2);
+  printf("%lf sense of the universe", decimalGrande);
 }
