659e697d7ea8ba6035f2f2cd98565fb6c43773c7
Emanuel Padilla <emanuehl159@gmail.com>
2024-11-15 22:47:02 -0500

    Add count using for loop in recall for c first steps file

--- recall/first-steps.c
+++ recall/first-steps.c
@@ -5,8 +5,17 @@ int num2 = 10;
 
 double decimalGrande = 3.333;
 
+void countFrom1toNumber(int Num);
+
 int main() {
   printf("hello there\n");
   printf("%d sense of the universe", num1 + num2);
   printf("%lf sense of the universe", decimalGrande);
+  countFrom1toNumber(num1);
+}
+
+void countFrom1toNumber(int num) {
+  for (int i = 1; i <= num; i++) {
+    printf("%i", i);
+  }
 }
