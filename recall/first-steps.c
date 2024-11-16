c9837d3d47416711c6098fd389934edc72a0e8f5
Emanuel Padilla <emanuehl159@gmail.com>
2024-11-15 23:12:03 -0500

    Add small example of appending a dynamic integer count to a file

--- recall/first-steps.c
+++ recall/first-steps.c
@@ -5,17 +5,26 @@ int num2 = 10;
 
 double decimalGrande = 3.333;
 
-void countFrom1toNumber(int Num);
+void countFrom1toNumber(int num);
+void writeNumToFile(int num, FILE *fpointer);
 
 int main() {
   printf("hello there\n");
-  printf("%d sense of the universe", num1 + num2);
-  printf("%lf sense of the universe", decimalGrande);
+  printf("%d sense of the universe\n", num1 + num2);
+  printf("%lf sense of the universe\n", decimalGrande);
   countFrom1toNumber(num1);
 }
 
 void countFrom1toNumber(int num) {
+  FILE *filePointer;
   for (int i = 1; i <= num; i++) {
-    printf("%i", i);
+    printf("%i\n", i);
+    writeNumToFile(i, filePointer);
   }
+}
+
+void writeNumToFile(int num, FILE *fpointer) {
+  fpointer = fopen("test.txt", "a");
+  fprintf(fpointer, "%i\n", num);
+  fclose(fpointer);
 }
